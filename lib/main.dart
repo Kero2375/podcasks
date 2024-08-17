2f1f4149e462777e2bdd06278a145b8d8ce778e4
Federico Carboni <f.carboni98@gmail.com>
2024-08-17 11:54:51 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:audio_service/audio_service.dart';
-import 'package:collection/collection.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
